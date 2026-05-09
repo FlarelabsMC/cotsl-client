2c408ed2821e457a894365d63e0cd2290a324d17
squoshi <95097230+squoshi@users.noreply.github.com>
2026-05-08 23:18:59 -0500

    temporal commit

--- src/main/java/com/flarelabsmc/cotsl/client/animation/PlayerDefaultAnimationHandler.java
+++ src/main/java/com/flarelabsmc/cotsl/client/animation/PlayerDefaultAnimationHandler.java
@@ -2,14 +2,17 @@ package com.flarelabsmc.cotsl.client.animation;
 
 import com.flarelabsmc.cotsl.common.CotSL;
 import com.flarelabsmc.cotsl.core.transform.duck.AbstractHorseDuck;
+import com.zigythebird.playeranim.animation.PlayerAnimResources;
 import com.zigythebird.playeranim.animation.PlayerAnimationController;
 import com.zigythebird.playeranim.animation.PlayerRawAnimationBuilder;
 import com.zigythebird.playeranim.api.PlayerAnimationAccess;
 import com.zigythebird.playeranimcore.animation.AnimationController;
 import com.zigythebird.playeranimcore.animation.AnimationData;
+import com.zigythebird.playeranimcore.animation.RawAnimation;
 import com.zigythebird.playeranimcore.animation.layered.modifier.AbstractFadeModifier;
 import com.zigythebird.playeranimcore.bones.AdvancedPlayerAnimBone;
 import com.zigythebird.playeranimcore.easing.EasingType;
+import com.zigythebird.playeranimcore.enums.PlayState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.Identifier;
@@ -25,6 +28,8 @@ import static com.zigythebird.playeranim.PlayerAnimLibMod.ANIMATION_LAYER_ID;
 
 @EventBusSubscriber(Dist.CLIENT)
 public class PlayerDefaultAnimationHandler {
+    private static boolean wasMoving;
+
     public static void init() {}
 
     @SubscribeEvent
@@ -36,8 +41,21 @@ public class PlayerDefaultAnimationHandler {
         Avatar a = controller.getAvatar();
         if (a.getVehicle() instanceof AbstractHorse horse) {
             AbstractHorseDuck hd = (AbstractHorseDuck) horse;
-            if (hd.getVel() > 0) {
-                Identifier horseAnim = Identifier.parse("cotsl:horse_ride");
+            boolean isMoving = hd.getVel() > 0;
+            if (isMoving && !wasMoving) {
+                if (controller.hasAnimationFinished()) wasMoving = true;
+                else {
+                    controller.triggerAnimation(Identifier.parse("cotsl:horse_start_moving"));
+                    controller.addModifierBefore(AbstractFadeModifier.standardFadeIn(10, EasingType.EASE_OUT_BACK));
+                    controller.setPostAnimationSetupConsumer(bonefunc -> {
+                        AdvancedPlayerAnimBone head = bonefunc.apply("head");
+                        head.rotYEnabled = false;
+                    });
+                    return;
+                }
+            }
+            if (isMoving && wasMoving) {
+                Identifier horseAnim = Identifier.parse("cotsl:horse_run");
                 if (!controller.isPlayingTriggeredAnimation()) {
                     controller.triggerAnimation(horseAnim);
                     controller.addModifierBefore(AbstractFadeModifier.standardFadeIn(10, EasingType.EASE_OUT_BACK));
@@ -45,9 +63,10 @@ public class PlayerDefaultAnimationHandler {
                         AdvancedPlayerAnimBone head = bonefunc.apply("head");
                         head.rotYEnabled = false;
                     });
+                    return;
                 }
-                return;
             }
+            wasMoving = isMoving;
         }
         controller.stopTriggeredAnimation();
     }
--- src/main/java/com/flarelabsmc/cotsl/common/entity/replaced/ReplacedHorse.java
+++ src/main/java/com/flarelabsmc/cotsl/common/entity/replaced/ReplacedHorse.java
@@ -5,18 +5,33 @@ import com.geckolib.animatable.instance.AnimatableInstanceCache;
 import com.geckolib.animatable.manager.AnimatableManager;
 import com.geckolib.animation.AnimationController;
 import com.geckolib.animation.RawAnimation;
-import com.geckolib.animation.object.PlayState;
 import com.geckolib.util.GeckoLibUtil;
+import net.minecraft.util.Mth;
 
 public class ReplacedHorse implements GeoReplacedEntity {
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
+    private boolean wasMoving;
+    private float speed;
+
+    public void setCurrentSpeed(float speed) {
+        this.speed = speed;
+    }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
         controllers.add(new AnimationController<>("horse_controller", 0, state -> {
-            if (state.isMoving()) return state.setAndContinue(RawAnimation.begin().thenPlay("animation.horse.run"));
-            return PlayState.STOP;
-        }));
+            state.setControllerSpeed(speed);
+            if (state.isMoving() && !wasMoving) {
+                if (state.controller().hasAnimationFinished()) wasMoving = true;
+                else return state.setAndContinue(RawAnimation.begin().thenPlay("animation.horse.start_moving"));
+            }
+            if (state.isMoving() && wasMoving)
+                return state.setAndContinue(RawAnimation.begin().thenPlay(speed > 0.05 ? "animation.horse.run" : "animation.horse.walk"));
+            // idle
+            wasMoving = state.isMoving();
+            speed = 1.0f;
+            return state.setAndContinue(RawAnimation.begin().thenPlay("animation.horse.idle"));
+        }).setTransitionTicks(10));
     }
 
     @Override
