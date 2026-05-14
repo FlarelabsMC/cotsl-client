9cd7587aadffa0fe70b295e1cc0c6313e6a448a1
Wayfarer <95097230+sentwayfarer@users.noreply.github.com>
2026-05-13 21:54:51 -0500

    fix

--- src/main/java/com/flarelabsmc/cotsl/launch/Paths.java
+++ src/main/java/com/flarelabsmc/cotsl/launch/Paths.java
@@ -16,10 +16,11 @@ public class Paths {
     public static File getInstallDir() {
         String os = System.getProperty("os.name", "").toLowerCase();
         String home = System.getProperty("user.home", ".");
+        String datahome = System.getenv("XDG_DATA_HOME");
         File dir;
         if (os.contains("win") && System.getenv("APPDATA") != null) dir = new File(System.getenv("APPDATA"), ".cotsl");
         else if (os.contains("mac")) dir = new File(home, "Library/Application Support/.cotsl");
-        else dir = new File(home + "/.local/share/", "cotsl");
+        else dir = new File(datahome != null ? datahome : home + "/.local/share/", "cotsl");
         dir.mkdirs();
         return dir;
     }
